64c269e96111fb66168d69b1fda4428d74807b30
Vlad Starkovsky <vlad@starkovsky.ru>
2017-08-22 01:47:34 +0300

    fix code style

--- src/assets/sass/pages/franchise/video-carousel/video-carousel.js
+++ src/assets/sass/pages/franchise/video-carousel/video-carousel.js
@@ -1,26 +1,31 @@
-jQuery( function( jQuery ) {
+jQuery(function (jQuery) {
 
-  var owlVideoCarousel  = jQuery( '.video-carousel__carousel.owl-carousel' );
+    var owlVideoCarousel = jQuery('.video-carousel__carousel.owl-carousel');
 
-  owlVideoCarousel.owlCarousel({
-      loop: true,
-      margin: 15,
-      items: 1,
-      nav: true,1
-      navContainer: '.video-carousel__nav',
-      onTranslate: carouselPagination
-  });
+    owlVideoCarousel.owlCarousel({
+        loop: true,
+        margin: 15,
+        items: 1,
+        nav: true, 1
+        navContainer
+:
+    '.video-carousel__nav',
+        onTranslate
+:
+    carouselPagination
+})
+    ;
 
-  function carouselPagination( event ) {
-    var carouselPageIndex = ( event.page.index + 1 ),
-    carouselPageCount = '/' + event.page.count;
+    function carouselPagination(event) {
+        var carouselPageIndex = ( event.page.index + 1 ),
+            carouselPageCount = '/' + event.page.count;
 
-    if ( carouselPageIndex <= 9 ) {
-      jQuery( '.video-carousel__nav-active-count' ).html( '0' + carouselPageIndex );
-    } else {
-      jQuery( '.video-carousel__nav-active-count' ).html( carouselPageIndex );
+        if (carouselPageIndex <= 9) {
+            jQuery('.video-carousel__nav-active-count').html('0' + carouselPageIndex);
+        } else {
+            jQuery('.video-carousel__nav-active-count').html(carouselPageIndex);
+        }
+        jQuery('.video-carousel__nav-total-count').html(carouselPageCount);
     }
-    jQuery( '.video-carousel__nav-total-count' ).html( carouselPageCount );
-  }
 
 });
