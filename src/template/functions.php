e2a80016604e7db3d2b80d610bd5b6c554cf7bd5
Vlad Starkovsky <vlad@starkovsky.ru>
2017-08-22 13:28:15 +0300

    disable usless func

--- src/template/functions.php
+++ src/template/functions.php
@@ -69,8 +69,6 @@ add_action('wp_enqueue_scripts', 'theme_files');
 
 
 
-
-
   // *----------------------------------------------------------------------- */
   // Изменяем длину вывода цитаты для поста excerpt
   function new_excerpt_length($length) {
@@ -85,7 +83,6 @@ add_action('wp_enqueue_scripts', 'theme_files');
 
 
 
-
   // *----------------------------------------------------------------------- */
   // Вывод пагинации
   $args_pagination = array(
@@ -93,7 +90,6 @@ add_action('wp_enqueue_scripts', 'theme_files');
     'next_text'    => __('Вперед »'),
   );
   // *----------------------------------------------------------------------- */
-
 
 
 
@@ -110,7 +106,6 @@ add_action('wp_enqueue_scripts', 'theme_files');
       return str_replace('<a href=', '<a '.$code.' href=', $output);
   }
   // *----------------------------------------------------------------------- */
-
 
 
 
@@ -372,5 +367,50 @@ add_action('wp_enqueue_scripts', 'theme_files');
     if ( isset( $_GET[ 'utm_content' ] ) && !isset( $_COOKIE[ 'utm_content' ] ) ) setcookie( 'utm_content', $_GET[ 'utm_content' ], time()+500000, '/' );
     if ( isset( $_GET[ 'utm_term' ] ) && !isset( $_COOKIE[ 'utm_term' ] ) ) setcookie( 'utm_term', $_GET[ 'utm_term' ], time()+500000, '/' );
   // *----------------------------------------------------------------------- */
+
+function disable_emojis()
+{
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+}
+add_action('init', 'disable_emojis');
+function disable_emojis_tinymce($plugins)
+{
+    if (is_array($plugins)) {
+        return array_diff($plugins, array('wpemoji'));
+    } else {
+        return array();
+    }
+}
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
+add_filter('rest_enabled', '__return_false');
+remove_action('xmlrpc_rsd_apis', 'rest_output_rsd');
+remove_action('wp_head', 'rest_output_link_wp_head', 10, 0);
+remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+remove_action('auth_cookie_malformed', 'rest_cookie_collect_status');
+remove_action('auth_cookie_expired', 'rest_cookie_collect_status');
+remove_action('auth_cookie_bad_username', 'rest_cookie_collect_status');
+remove_action('auth_cookie_bad_hash', 'rest_cookie_collect_status');
+remove_action('auth_cookie_valid', 'rest_cookie_collect_status');
+remove_filter('rest_authentication_errors', 'rest_cookie_check_errors', 100);
+remove_action('init', 'rest_api_init');
+remove_action('rest_api_init', 'rest_api_default_filters', 10, 1);
+remove_action('parse_request', 'rest_api_loaded');
+remove_action('rest_api_init', 'wp_oembed_register_route');
+remove_filter('rest_pre_serve_request', '_oembed_rest_pre_serve_request', 10, 4);
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'wp_resource_hints', 2);
+
 
 ?>
