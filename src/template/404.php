336f1371ddbb9d6414377504532d1c921fd8bd7e
Vlad Starkovsky <vlad@starkovsky.ru>
2017-08-27 21:33:47 +0300

    refactoring

--- src/template/404.php
+++ src/template/404.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <head>
+<head>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
-  	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-  	<title><?php wp_title('-', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-  	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-  	<?php wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?>>
-    <div class="container error404__container">
-      <div class="row error404__row align-item-center">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div class="container error404__container">
+    <div class="row error404__row align-item-center">
         <div class="col-12">
-          <h1 class="error404__title error404__title_text-shadow">404</h1>
-          <div class="error404__sub-title error404__sub-title-shadow">страница не найдена :(</div>
-          <div class="error404__description">Сожалеем, но вы нашли неработающую страницу или ссылку сайта. Напишите в отдел поддержки на <a href="mailto:support@mossebo.ru">support@mossebo.ru</a>, и мы поможем найти потерянное!</div>
-          <div class="text-right">
-            <a href="<?php echo get_home_url(); ?>" class="link-arrow error404__link-arrow">Перейти на главную</a>
-          </div>
+            <h1 class="error404__title error404__title_text-shadow">404</h1>
+            <div class="error404__sub-title error404__sub-title-shadow">страница не найдена :(</div>
+            <div class="error404__description">Сожалеем, но вы нашли неработающую страницу или ссылку сайта. Напишите в
+                отдел поддержки на <a href="mailto:support@mossebo.ru">support@mossebo.ru</a>, и мы поможем найти
+                потерянное!
+            </div>
+            <div class="text-right">
+                <a href="<?php echo get_home_url(); ?>" class="link-arrow error404__link-arrow">Перейти на главную</a>
+            </div>
         </div>
-      </div>
     </div>
-  </body>
+</div>
+</body>
 </html>
--- src/template/page.php
+++ src/template/page.php
@@ -1,30 +1,30 @@
 <?php get_header(); ?>
 
-  <div class="container-fluid">
+<div class="container-fluid">
 
     <!-- Title -->
     <div class="row">
-      <div class="col-12 text-center">
-        <h1 class="section-title news-page-title"><?php echo get_the_title(); ?></h1>
-      </div>
+        <div class="col-12 text-center">
+            <h1 class="section-title news-page-title"><?php echo get_the_title(); ?></h1>
+        </div>
     </div>
 
     <!-- Post -->
     <div class="row">
-      <div class="news-page-wrapper">
-        <div class="col-12">
-          <?php
-          if ( have_posts() ) {
-          	while ( have_posts() ) {
-          		the_post();
-              the_content();
-          	} // end while
-          } // end if
-          ?>
+        <div class="news-page-wrapper">
+            <div class="col-12">
+                <?php
+                if (have_posts()) {
+                    while (have_posts()) {
+                        the_post();
+                        the_content();
+                    } // end while
+                } // end if
+                ?>
+            </div>
         </div>
-      </div>
     </div>
 
-  </div><!-- Container fluid END -->
+</div><!-- Container fluid END -->
 
 <?php get_footer(); ?>
