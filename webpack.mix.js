f378c00d3a63f6601d358799a16829f2056dd7ed
Vlad Starkovsky <vladstarkovsky@iMac-Vlad.local>
2017-10-11 12:12:16 +0300

    Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,34 +11,21 @@ let mix = require('laravel-mix');
  |
  */
 
-/*mix.js('src/assets/js/app.js', 'dist/assets/js');
-mix.sass('src/assets/sass/app.scss', 'dist/assets/css')
+publicDir = '../Projects/mossebo.dev/public_html/wp-content/themes/mossebo';
+
+mix.js('src/assets/js/app.js', publicDir + '/assets/js');
+mix.sass('src/assets/sass/app.scss', publicDir + '/assets/css')
     .options({
         processCssUrls: false,
     });
-mix.copyDirectory('src/assets/fonts', 'dist/assets/fonts');
-mix.copyDirectory('src/assets/images', 'dist/assets/images');
+mix.copyDirectory('src/assets/fonts', publicDir + '/assets/fonts');
+mix.copyDirectory('src/assets/images', publicDir + '/assets/images');
 mix.autoload({
     jquery: ['$', 'window.jQuery', 'jQuery']
 });
 mix.sourceMaps();
-mix.setPublicPath('dist');
-mix.copyDirectory('src/template', 'dist');*/
-
-
-mix.js('src/assets/js/app.js', '../hs-project/mossebo.dev/wp-content/themes/mossebo/assets/js');
-mix.sass('src/assets/sass/app.scss', '../hs-project/mossebo.dev/wp-content/themes/mossebo/assets/css')
-    .options({
-        processCssUrls: false,
-    });
-mix.copyDirectory('src/assets/fonts', '../hs-project/mossebo.dev/wp-content/themes/mossebo/assets/fonts');
-mix.copyDirectory('src/assets/images', '../hs-project/mossebo.dev/wp-content/themes/mossebo/assets/images');
-mix.autoload({
-    jquery: ['$', 'window.jQuery', 'jQuery']
-});
-// mix.sourceMaps();
-mix.setPublicPath('../hs-project/mossebo.dev/wp-content/themes/mossebo');
-mix.copyDirectory('src/template', '../hs-project/mossebo.dev/wp-content/themes/mossebo');
+mix.setPublicPath(publicDir);
+mix.copyDirectory('src/template', publicDir);
 mix.browserSync({
     proxy: {
         target: "https://mossebo.dev"
