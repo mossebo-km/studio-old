3e7f09ae096ae44ff95d154db744ef991894d952
Vlad Starkovsky <vlad@starkovsky.ru>
2017-10-13 19:08:51 +0300

    update #13.10.2017 #1

--- src/template/co-work-production.php
+++ src/template/co-work-production.php
@@ -200,10 +200,186 @@ get_header();
     <div class="row">
         <div class="col-xl-10 offset-xl-1 col-12">
             <div class="row">
+                <div class="title">Instagram площадки Mossebo</div>
+                <div class="subtitle">Получайте новых клиентов через наши площадки</div>
+                <div class="row instagrams">
+                    <div class="col-md-4">
+                        <img src="//mossebo.studio/wp-content/uploads/original/logos/2-1.png" alt="">
+                    </div>
+                    <div class="col-md-4">
+                        <img src="//mossebo.studio/wp-content/uploads/original/logos/2-2.png" alt="">
+                    </div>
+                    <div class="col-md-4">
+                        <img src="//mossebo.studio/wp-content/uploads/original/logos/2-3.png" alt="">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+
+<div class="co-work-production-inventary container-fluid">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1 col-12">
+            <div class="row">
+                <div class="col-md-6">
+                    <div>
+                        <strong class="co-work-production-inventary__title">Современное оборудование</strong>
+                        <p class="co-work-production-inventary__subtitle">Снимаем сюжеты о вашем бренде на лучшую технику.
+                            <br><br>
+
+                            Камеры Panasonic GH-4 и Sony α7 II с набором объективов, электронный трехосевой стадикам
+                            Beholder и звук Rode… Можно долго перечислять, но главное, что финальная картинка вас впечатлит.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div>
+                        <img src="//mossebo.studio/wp-content/uploads/inv-1.jpg" alt=""
+                             class="co-work-production-inventary__photo">
+                        <img src="//mossebo.studio/wp-content/uploads/inv-2.jpg" alt=""
+                             class="co-work-production-inventary__photo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="co-work-production-stats container-fluid">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1 col-12">
+                <div class="title">Статистика аудитории бренда Mossebo</div>
+            <div class="stat-1"><span> > </span>4 500 000<small>просмотров</small></div>
+            <div class="row stat-2">
+                <div class="col-md-1">
+                    <img src="//mossebo.studio/wp-content/uploads/original/logos/3-1.png" alt="">
+                </div>
+                <div class="col-md-3">
+                    <strong>80%</strong>
+                    <span>аудитории из России</span>
+                </div>
+                <div class="col-md-1">
+                    <img src="//mossebo.studio/wp-content/uploads/original/logos/3-2.png" alt="">
+                </div>
+                <div class="col-md-3">
+                    <strong>90%</strong>
+                    <span>видео выходит в тренды YouTube</span>
+                </div>
+                <div class="col-md-1">
+                    <img src="//mossebo.studio/wp-content/uploads/original/logos/3-3.png" alt="">
+                </div>
+                <div class="col-md-3">
+                    <strong>72.5%</strong>
+                    <span>аудитории в возрасте от 18 до 34 лет</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="co-work-production-gallery container-fluid">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1 col-12">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="//mossebo.studio/wp-content/uploads/original/4-1.jpg" alt="">
+                </div>
+                <div class="col-md-4">
+                    <img src="//mossebo.studio/wp-content/uploads/original/4-2.jpg" alt="">
+                </div>
+                <div class="col-md-4">
+                    <img src="//mossebo.studio/wp-content/uploads/original/4-3.jpg" alt="">
+                </div>
+                <div class="col-md-4">
+                    <img src="//mossebo.studio/wp-content/uploads/original/4-4.jpg" alt="">
+                </div>
+                <div class="col-md-4">
+                    <img src="//mossebo.dev/wp-content/themes/mossebo/assets/images/header/logo-mossebo.svg" alt="">
+                </div>
+                <div class="col-md-4">
+                    <img src="//mossebo.studio/wp-content/uploads/original/4-5.jpg" alt="">
+                </div>
+                <div class="col-md-4">
+                    <img src="//mossebo.studio/wp-content/uploads/original/4-6.jpg" alt="">
+                </div>
+                <div class="col-md-4">
+                    <img src="//mossebo.studio/wp-content/uploads/original/4-7.jpg" alt="">
+                </div>
+                <div class="col-md-4">
+                    <img src="//mossebo.studio/wp-content/uploads/original/4-8.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="co-work-production-person container-fluid">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1 col-12">
+            <div class="title">Медийные лица Mossebo</div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="person_name">Марк <br>Еремин</div>
+                    <img src="//mossebo.studio/wp-content/uploads/original/mark.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <div class="person_name">Юрий <br>Еремин</div>
+                    <img src="//mossebo.studio/wp-content/uploads/original/yury.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <div class="person_name">Владислав <br>Туржанский</div>
+                    <img src="//mossebo.studio/wp-content/uploads/original/vlad.jpg" alt="">
+                </div>
+                <div class="col-md-3">
+                    <div class="person_name">Liza <br>Loft</div>
+                    <img src="//mossebo.studio/wp-content/uploads/original/liza.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="co-work-production-inventary container-fluid">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1 col-12">
+            <div class="row">
+                <div class="col-md-6">
+                    <div>
+                        <strong class="co-work-production-inventary__title">Лайв-трансляции</strong>
+                        <p class="co-work-production-inventary__subtitle">В офисе Mossebo в Санкт-Петербурге оборудована студия для прямых трансляций. В конце каждого месяца мы разыгрываем все подарки от гостей за месяц. </p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div>
+                        <img src="//mossebo.studio/wp-content/uploads/live-1.jpg" alt=""
+                             class="co-work-production-inventary__photo">
+                        <img src="//mossebo.studio/wp-content/uploads/live-2.jpg" alt=""
+                             class="co-work-production-inventary__photo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="co-work-production-pay container-fluid">
+    <div class="row">
+        <div class="col-xl-10 offset-xl-1 col-12">
+            <strong class="co-work-production-pay__title">Все формы оплаты</strong>
+            <p class="co-work-production-pay__subtitle">Оплата рекламы и интеграций возможны на расчетный счет или на карту Сбербанка.</p>
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <div class="co-work-production-leadform container-fluid">
     <div class="row">
